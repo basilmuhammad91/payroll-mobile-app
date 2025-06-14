460e609836f27c0b395b4b60ed740ded35a6c825
basilmuhammad91 <basilmuhammad91@gmail.com>
2025-06-14 16:34:23 +0500

    =fixing

--- app/(tabs)/leave.jsx
+++ app/(tabs)/leave.jsx
@@ -1,20 +1,11 @@
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { Image } from 'expo-image';
-import { StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 import FormScreen from '../../screens/FormScreen';
 
 export default function TabTwoScreen() {
   return (
-    <ParallaxScrollView
-          headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-          headerImage={
-            <Image
-              source={require('@/assets/images/partial-react-logo.png')}
-              style={styles.reactLogo}
-            />
-          }>
+    <ScrollView>
         <FormScreen />
-    </ParallaxScrollView>
+    </ScrollView>
   );
 }
 
